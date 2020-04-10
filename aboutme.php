34aa09b6503c756647a405037868328c8410d3ac
Raid Tammam <42742321+raidtammam@users.noreply.github.com>
2020-04-11 05:55:54 +0700

    kedua

--- aboutme.php
+++ aboutme.php
@@ -1,4 +1,5 @@
 echo "Nama : raidtammam <br>";
 echo "Kelas : XI Rpl 1 <br>";
 echo "Asal Sekola : SMK Taruna Bhakti <br>";
-echo "Alamat : Depok <br>";
+echo "NoHp : 0852177969xxx";
+echo "Email : raidtammam10@gmail.com";
